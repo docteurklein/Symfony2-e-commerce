dd28100f30016a70533edfc1ada553375a4a5177
Klein Florian <florian.klein@free.fr>
2010-12-13 01:06:19 +0100

    splitted DI config file in well named files

--- src/Bundle/ECommerce/ProductBundle/DependencyInjection/ProductExtension.php
+++ src/Bundle/ECommerce/ProductBundle/DependencyInjection/ProductExtension.php
@@ -7,12 +7,52 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+use Doctrine\ODM\MongoDB\Query\Builder;
+
 class ProductExtension extends Extension
 {
     public function configLoad(array $config, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
-        $loader->load('product.xml');
+        $loader->load('model.xml');
+        $loader->load('paginator.xml');
+        $loader->load('menu.xml');
+    }
+
+    /**
+     * Get a DocumentRepository
+     *
+     * @param mixed $objectManager a DocumentManager
+     * @param mixed $objectClass the class of the document
+     * @return DocumentRepository
+     */
+    public static function getRepository($objectManager, $objectClass)
+    {
+        return $objectManager->getRepository($objectClass);
+    }
+
+    /**
+     * Get a closure that instanciates a paginator
+     *
+     * Call it like this:
+     *   $builder = $this->get('ecommerce.repository.product')->getBuilder();
+     *   $pagerFactory = $this->get('ecommerce.paginator.product.factory');
+     *   $pager = \call_user_func($pagerFactory, $builder);
+     *   // or
+     *   $pager = $pagerFactory($builder);
+     *
+     * @static
+     * @param  $paginatorClass the paginator class name
+     * @param  $paginatorAdapterClass the adapter class name
+     * @return Closure the closure to invoke with a Query builder as first argument
+     */
+    public static function getPaginatorFactory($paginatorClass, $paginatorAdapterClass)
+    {
+        return function(Builder $builder) use ($paginatorClass, $paginatorAdapterClass) {
+            $adapter = new $paginatorAdapterClass($builder);
+            
+            return new $paginatorClass($adapter);
+        };
     }
 
     /**
--- src/Bundle/ECommerce/ProductBundle/Resources/views/Product/index.php
+++ src/Bundle/ECommerce/ProductBundle/Resources/views/Product/index.php
@@ -3,7 +3,7 @@
 <h1><?php echo $view['translator']->trans('Products') ?></h1>
 
 <ul>
-<?php foreach($products as $product): ?>
+<?php foreach($pager->getCurrentItems() as $product): ?>
     <li><a href="<?php echo $view['router']->generate('product_show', array('slug' => $product->getSlug())) ?>"><?php echo $product ?></a></li>
 <?php endforeach ?>
 </ul>
